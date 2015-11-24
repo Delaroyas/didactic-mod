c3c4c1383b111b9be98d860f2ec4df1738d0e9fa
Delaroyas <github.finnerty@ptaff.ca>
2015-11-23 20:23:45 -0500

    giveitems replaces givenodes

--- command_test.lua
+++ command_test.lua
@@ -1,24 +1,75 @@
+minetest.register_chatcommand("giveitems", {
+	privs = {
+		interact = true
+	},
+	func = function(playername, modname, page)
+		local ind=0;
+		local iSize=32
+
+	modpagei = modpagei or {}
+	
+	modpagei[modname] = modpagei[modname] or 0
+	modpagei[modname] = modpagei[modname]+1
+
+		local page = modpagei[modname]
+
+
+		minetest.chat_send_player(playername,  "Page = " .. page .. "(" .. type(page) ..")")
+		player=minetest.get_player_by_name(playername)
+		for key,item in pairs(minetest.registered_items) do
+			if item.name:find(modname)==1 then
+				ind=ind+1;
+				if ind > iSize*page then
+				     return true, " Type '/giveitems ".. modname .. " " .. (page+1) .. "' for the rest. "	
+				elseif ind > iSize*(page-1) then
+					player:get_inventory():add_item('main', item.name)
+				end
+				--minetest.chat_send_player(playername, key .. " = " .. item.name)
+			end		
+		end
+		modpagei[modname]=nil
+		return true, " You have all the items !"
+		
+	end
+})
+
+--[[ May be obsolete, giveitems gives blocs and items...
+
 minetest.register_chatcommand("givenodes", {
 	privs = {
 		interact = true
 	},
-	func = function(playername, itemname, page)
+	func = function(playername, modname, page)
 		local ind=0;
 		local iSize=32
-		local page = page or 1
+
+	modpage = modpage or {}
+	
+	modpage[modname] = modpage[modname] or 0
+	modpage[modname] = modpage[modname]+1
+
+		local page = modpage[modname]
+
+
 		minetest.chat_send_player(playername,  "Page = " .. page .. "(" .. type(page) ..")")
-		for key,value in pairs(minetest.registered_nodes) do
-			player=minetest.get_player_by_name(playername)
-			if value.name:find(itemname)==1 then
+		player=minetest.get_player_by_name(playername)
+					
+		for key,item in pairs(minetest.registered_nodes) do
+			if item.name:find(modname)==1 then
 				ind=ind+1;
 				if ind > iSize*page then
-				     return true, " Type '/givenodes ".. itemname .. " " .. (page+1) .. "' for the rest. "	
+				     return true, " Type '/givenodes ".. modname .. " " .. (page+1) .. "' for the rest. "	
 				elseif ind > iSize*(page-1) then
-					player:get_inventory():add_item('main', value.name)
+					player:get_inventory():add_item('main', item.name)
 				end
-				--minetest.chat_send_player(playername, key .. " = " .. value.name)
+				--minetest.chat_send_player(playername, key .. " = " .. item.name)
 			end		
 		end
-		return true, " You have all the items !"
+		modpagei[modname]=nil
+		return true, " You have all the nodes !"
+
 	end
 })
+--]]
+
+
