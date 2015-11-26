ef5fae706110f543d95262f979bc5ee7ca8687e7
Delaroyas <github.finnerty@ptaff.ca>
2015-11-25 19:56:58 -0500

    Added last mod date.

--- dataio_test.lua
+++ dataio_test.lua
@@ -1,4 +1,4 @@
-local function load_data(filename)
+function load_data(filename)
    local file = io.open(filename, "r")
    if file then
       local table = minetest.deserialize(file:read("*all"))
@@ -9,6 +9,26 @@ local function load_data(filename)
    end   
 end
 
+local function load_player_moddata(playername,modname) 
+	filename=minetest.get_worldpath().."/players/"..playername.."_".. modname..".dat";
+	local thisdata = load_data(filename) or {} -- loads file if it exists, or makes empty table
+	minetest.chat_send_all(" Loaded:"..filename)
+	return thisdata
+end
+
+minetest.register_on_joinplayer(function(player)
+	blockpunches = blockpunches or {}
+	local playername= player:get_player_name()
+	mydata=mydata or {}
+	mydata[playername]=load_player_moddata(playername,'mydata') 
+	mydata[playername].blockpunches=mydata[playername].blockpunches or 0 
+end)
+
+
+
+
+
+
 local function save_data(filename, data)
    local file = io.open(filename, "w")
    if file then
@@ -17,47 +37,31 @@ local function save_data(filename, data)
    end
 end
 
-
-local function load_player_moddata(playername,modname) 
-	filename=minetest.get_worldpath().."/"..modname.."_"..playername..".dat";
-	local thisdata = load_data(filename) or {} -- loads file if it exists, or makes empty table
-	minetest.chat_send_all(" Loaded:"..filename)
-	return thisdata
-end
 local function save_player_moddata(playername,modname,mydata)
-	
-	filename=minetest.get_worldpath().."/"..modname.."_"..playername..".dat";
+	filename=minetest.get_worldpath().."/players/"..playername.."_".. modname..".dat";
 	save_data(filename, mydata)
 	minetest.chat_send_all(" Saved:"..filename)
 end
-
-
-
-
-
-minetest.register_on_leaveplayer(function(player)
+local function save_mydata(player)
 	local playername= player:get_player_name()
-	minetest.chat_send_all(playername.." has left this awesome game.")
+	mydata[playername].lastsave=os.date("%Y-%m-%d %H:%M:%S");
 	save_player_moddata(playername,'mydata',mydata[playername])
-end)
-
-minetest.register_on_joinplayer(function(player)
-	blockpunches = blockpunches or {}
-	local playername= player:get_player_name()
-	mydata=mydata or {}
-	mydata[playername]=load_player_moddata(playername,'mydata') 
-	mydata[playername].blockpunches=mydata[playername].blockpunches or 0 
-	minetest.chat_send_all(playername.." has joined this awesome game.")
-end)
-
-minetest.register_on_shutdown(function()
-
-for _,player in ipairs(minetest.get_connected_players()) do
-	local playername = player:get_player_name()
-	save_player_moddata(playername,'mydata',mydata[playername])
+	mydata[playername]=nil
 end
 
 
+minetest.register_on_leaveplayer(function(player)
+	save_mydata(player)
 end)
+
+minetest.register_on_shutdown(function()
+	for _,player in ipairs(minetest.get_connected_players()) do
+		save_mydata(player)
+	end
+end)
+
+
+
+
 
 
