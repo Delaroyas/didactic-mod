b1b2eec0d9b3f0d19a0dbfe3358a522757873877
Delaroyas <github.finnerty@ptaff.ca>
2015-11-25 19:28:52 -0500

    save

--- countpunch_test.lua
+++ countpunch_test.lua
@@ -1,14 +1,11 @@
 minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
 	--initialise blockpunches as needed	
-	blockpunches = blockpunches or {}
+	--blockpunches = blockpunches or {}
 	
 	local puncher_name = puncher:get_player_name()
-	blockpunches[puncher_name] = blockpunches[puncher_name] or 0
-	blockpunches[puncher_name] = blockpunches[puncher_name]+1	
-	local puncher_name = puncher:get_player_name()
-		
-
-	minetest.chat_send_all("Hey!! ".. puncher_name .." as punched ".. blockpunches[puncher_name] .." blocks!!")		
+	mydata[puncher_name].blockpunches = mydata[puncher_name].blockpunches+1
+	
+	minetest.chat_send_all("Hey!! ".. puncher_name .." as punched ".. mydata[puncher_name].blockpunches .." blocks!!")		
 	
 
 end)
--- init.lua
+++ init.lua
@@ -10,4 +10,5 @@ dofile(minetest.get_modpath("didactic_mod").."/command_test.lua")
 --onpunch: Unse only one of those:
 --dofile(minetest.get_modpath("didactic_mod").."/onpunch_test.lua")
 dofile(minetest.get_modpath("didactic_mod").."/countpunch_test.lua")
+dofile(minetest.get_modpath("didactic_mod").."/dataio_test.lua")
 
